99ecba816b11bccb2bc748577094fd478e1e5b6e
Matthew <matthew_massey@hotmail.com>
2020-01-25 21:19:46 -0500

    added surpassed component

---
+++ smart-schedule/src/app/home/surpassed/surpassed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-surpassed',
+  templateUrl: './surpassed.component.html',
+  styleUrls: ['./surpassed.component.scss']
+})
+export class SurpassedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
